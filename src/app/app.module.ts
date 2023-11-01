8ed9772af1e84bc6af92ae77413591739e9f25a9
Felipe <riopipe8@gmail.com>
2023-11-01 18:06:07 -0300

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { ProductImagesComponent } from './components/product-images/product-images.component';
+import { ProductInfoComponent } from './components/product-info/product-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductImagesComponent,
+    ProductInfoComponent
   ],
   imports: [
     BrowserModule,
