9cd87b502cf05b4e231a5290aebc70b5703d806b
Felipe <riopipe8@gmail.com>
2023-11-07 17:22:08 -0300

    images fixed

--- src/app/mock/product_example.ts
+++ src/app/mock/product_example.ts
@@ -9,15 +9,15 @@ export const productExample: Product = {
     price: 250,
     saleOffPrice: 125,
     images: [
-        "../../assets/images/image-product-1.jpg",
-        "../../assets/images/image-product-2.jpg",
-        "../../assets/images/image-product-3.jpg",
-        "../../assets/images/image-product-4.jpg"
+        "./assets/images/image-product-1.jpg",
+        "./assets/images/image-product-2.jpg",
+        "./assets/images/image-product-3.jpg",
+        "./assets/images/image-product-4.jpg"
     ],
     imagesSmall: [
-        "../../assets/images/image-product-1-thumbnail.jpg",
-        "../../assets/images/image-product-2-thumbnail.jpg",
-        "../../assets/images/image-product-3-thumbnail.jpg",
-        "../../assets/images/image-product-4-thumbnail.jpg"
+        "./assets/images/image-product-1-thumbnail.jpg",
+        "./assets/images/image-product-2-thumbnail.jpg",
+        "./assets/images/image-product-3-thumbnail.jpg",
+        "./assets/images/image-product-4-thumbnail.jpg"
     ]
 }
